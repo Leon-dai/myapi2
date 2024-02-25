6abb891b0fde30049a962f5117c82b8ffc1739a9
Leon <212649354@qq.com>
2024-02-25 23:22:47 +0800

    Lambda表达式的省略写法小练习

---
+++ src/com/itheima/lamdademo/LambdaDemo4.java
@@ -0,0 +1,20 @@
+package com.itheima.lamdademo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LambdaDemo4 {
+    public static void main(String[] args) {
+        /*
+            定义数组并存储一些字符串，利用Arrays中的sort方法进行排序
+            要求：
+                按照字符串的长度进行排序，短的在前面，长的在后面。
+                （暂时不比较字符串里面的内容）
+         */
+
+        String[] arr = {"a", "aaaa", "aaa", "aa"};
+        Arrays.sort(arr, (o1, o2) -> o1.length() - o2.length());
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
